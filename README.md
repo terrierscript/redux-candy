66767b1e1bf393198f385ca8ddec94c263d681cc
inuscript <inuscript@gmail.com>
2017-01-01 02:16:16 +0900

    add example

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # redux-candy 🍭
 *HIGHLY EXPERIMENTAL*
 
+Very 
+
 ## Example
+
+```js
+const initialState = {
+  counter: 0
+}
+const reducer = createReducer(initialState)
+
+// counter
+const increment = createReducerAction('INCREMENT', 'counter', () => ( (i) => i + 1 ))
+const decrement = createReducerAction('DECREMENT', 'counter', () => ( (i) => i - 1 ))
+
+const Counter = ({dispatch, counter}) => {
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+    </div>
+  )
+}
+
+const CounterContainer = connect(state => state)(Counter)
+const store = createStore(reducer)
+const App = () => (
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>
+)
+```
--- test/createReducerAction.js
+++ test/createReducerAction.js
@@ -7,57 +7,58 @@ const emulateState = (initialState, action) => {
   const mockReducer = createReducer()
   return mockReducer(initialState, action)
 }
+describe("redux-sweet", () => {
+  it("with plain action", () => {
+    const actionCreator = (text) => ({
+      type: 'ADD_TODO',
+      payload: {
+        todos: (todos) => [ ...todos, text ]
+      }
+    })
+    const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
+    expect(actualState).toEqual({
+      todos: ["foo", "baz"]
+    })
+  })
 
-it("with plain action", () => {
-  const actionCreator = (text) => ({
-    type: 'ADD_TODO',
-    payload: {
-      todos: (todos) => [ ...todos, text ]
-    }
+  it("with redux-actions/createAction", () => {
+    const actionCreator = createAction('ADD_TODO', (text) => ({
+      todos: (todos) => {
+        return [ ...todos, text ]
+      }
+    }) )
+    const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
+    expect(actualState).toEqual({
+      todos: ["foo", "baz"]
+    })
   })
-  const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
-  expect(actualState).toEqual({
-    todos: ["foo", "baz"]
-  })
-})
 
-it("with redux-actions/createAction", () => {
-  const actionCreator = createAction('ADD_TODO', (text) => ({
-    todos: (todos) => {
-      return [ ...todos, text ]
-    }
-  }) )
-  const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
-  expect(actualState).toEqual({
-    todos: ["foo", "baz"]
+  it("createReduceAction", () => {
+    const actionCreator = createReducerAction('ADD_TODO', 'todos', (text) => (
+      (todos) => [ ...todos, text ]
+    ))
+    const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
+    expect(actualState).toEqual({
+      todos: ["foo", "baz"]
+    })
   })
-})
 
-it("createReduceAction", () => {
-  const actionCreator = createReducerAction('ADD_TODO', 'todos', (text) => (
-    (todos) => [ ...todos, text ]
-  ))
-  const actualState = emulateState({ todos: ["foo"] }, actionCreator("baz"))
-  expect(actualState).toEqual({
-    todos: ["foo", "baz"]
+  it("replaceValue", () => {
+    const actionCreator = createReducerAction('ADD_TODO', 'someValue')
+    const actualState = emulateState({ someValue: "zoo" }, actionCreator("bee"))
+    expect(actualState).toEqual({
+      someValue: "bee"
+    })
   })
-})
 
-it("replaceValue", () => {
-  const actionCreator = createReducerAction('ADD_TODO', 'someValue')
-  const actualState = emulateState({ someValue: "zoo" }, actionCreator("bee"))
-  expect(actualState).toEqual({
-    someValue: "bee"
-  })
-})
-
-it("removeValue", () => {
-  const actionCreator = createReducerAction('ADD_TODO', 'someValue', () => {
-    return undefined
-  })
-  const actualState = emulateState({ someValue: "zoo", fooValue: "boo" }, actionCreator())
-  expect(actualState).toEqual({
-    someValue: undefined,
-    fooValue: "boo"
+  it("removeValue", () => {
+    const actionCreator = createReducerAction('ADD_TODO', 'someValue', () => {
+      return undefined
+    })
+    const actualState = emulateState({ someValue: "zoo", fooValue: "boo" }, actionCreator())
+    expect(actualState).toEqual({
+      someValue: undefined,
+      fooValue: "boo"
+    })
   })
 })
