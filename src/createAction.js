7bcf75570a987a5a628c3a01ce3842bf57eb952e
inuscript <inuscript@gmail.com>
2017-01-01 12:21:30 +0900

    lint

--- src/createAction.js
+++ src/createAction.js
@@ -2,7 +2,7 @@ import { createAction } from 'redux-actions'
 
 const identity = value => value
 
-export default function (type, key, updateFunction = identity){
+export default function (type, key, updateFunction = identity) {
   const payloadCreator = (...params) => {
     return { [key]: updateFunction(...params) }
   }
--- src/createReducer.js
+++ src/createReducer.js
@@ -1,11 +1,12 @@
 import updeep from 'updeep'
 
-const defaultUpdateCondition = ( { payload } ) => {
-  return !!(typeof payload === "object")
+const defaultUpdateCondition = ({ payload }) => {
+  return !!(typeof payload === 'object')
 }
-export default function(initialState, updateConditon = defaultUpdateCondition) {
-  return (state = initialState, action ) => {
-    if(!updateConditon(action)){
+
+export default function (initialState, updateConditon = defaultUpdateCondition) {
+  return (state = initialState, action) => {
+    if (!updateConditon(action)) {
       return state
     }
     return updeep(action.payload, state)
