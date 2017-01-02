794197c5a63bdaf7c2e74f490b6ac6db632e2614
inuscript <inuscript@gmail.com>
2017-01-02 22:17:06 +0900

    change nest function

---
+++ src/compose.js
@@ -0,0 +1,7 @@
+
+// port from redux
+export default function compose (...funcs) {
+  if (funcs.length === 0) return arg => arg
+  if (funcs.length === 1) return funcs[0]
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
--- src/nest.js
+++ src/nest.js
@@ -1,28 +1,19 @@
-import identity from 'lodash/identity'
-import isFunction from 'lodash/isFunction'
-import invariant from 'invariant'
-import isNull from 'lodash/isNull'
+import compose from './compose'
 
-function wrapPayloadCreator (key, payloadCreator = identity) {
-  invariant(
-    isFunction(payloadCreator) || isNull(payloadCreator),
-    'Expected payloadCreator to be a function, undefined or null'
-  )
-  return function (...params) {
-    return { [key]: payloadCreator(...params) }
+const curried = (key, convert) => {
+  return (payloadCreator) => (...input) => {
+    return convert(key, payloadCreator, ...input)
   }
 }
 
-// port from redux
-function compose (...funcs) {
-  if (funcs.length === 0) return arg => arg
-  if (funcs.length === 1) return funcs[0]
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+export const nest = (convert) => {
+  return (...keys) => {
+    const nestFuncs = keys.map(
+      (key) => curried(key, convert)
+    )
+    return compose(...nestFuncs)
+  }
 }
 
-export default function nest (...keys) {
-  const nestFuncs = keys.map(
-    (key) => (updateFunction) => wrapPayloadCreator(key, updateFunction)
-  )
-  return compose(...nestFuncs)
-}
+
+export default nest
